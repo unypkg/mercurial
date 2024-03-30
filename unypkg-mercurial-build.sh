9ac819a2df27698c0e119865322488e7963a1e38
michacassola <mich@casso.la>
2024-03-30 15:25:54 +0100

    Update unypkg-mercurial-build.sh

--- unypkg-mercurial-build.sh
+++ unypkg-mercurial-build.sh
@@ -36,7 +36,7 @@ source_tarball_basename="$(
         tail -n 1
 )"
 source_tarball_url="https://mercurial-scm.org/release/$source_tarball_basename"
-source_folder_name="${source_tarball_basename//\.tar.*/}"
+#source_folder_name="${source_tarball_basename//\.tar.*/}"
 latest_ver="$(echo $source_tarball_basename | grep -oE "[0-9]*(([0-9]+\.)*[0-9]+)")"
 latest_commit_id="$latest_ver"
 
@@ -61,9 +61,7 @@ check_for_repo_and_create
 wget "$source_tarball_url"
 tar xfz "$source_tarball_basename"
 rm "$source_tarball_basename"
-mv "$source_folder_name" mercurial
-
-archiving_source
+XZ_OPT="--threads=0" tar -cJpf "$pkgname-$latest_ver".tar.xz "$pkgname-$latest_ver"
 
 ######################################################################################################################
 ### Build
