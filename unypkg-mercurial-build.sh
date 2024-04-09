3994d77a1c14492c7977d5e039bbe183faf1c26b
michacassola <mich@casso.la>
2024-04-09 16:11:23 +0200

    Update unypkg-mercurial-build.sh

--- unypkg-mercurial-build.sh
+++ unypkg-mercurial-build.sh
@@ -13,6 +13,8 @@ mkdir /uny/tmp
 unyp install openssl
 unyp install libffi
 unyp install python
+pip3_bin=(/uny/pkg/python/*/bin/pip3)
+"${pip3_bin[0]}" install setuptools
 
 ### Getting Variables from files
 UNY_AUTO_PAT="$(cat UNY_AUTO_PAT)"
